9d048a9c0bec634c35f74555942833b4998bc599
Marcio Junior <mps.junior@hotmail.com>
2026-06-22 19:56:29 -0300

    adicionando comentário

--- 2_FUNDAMENTOS/9_lendo_input/index.js
+++ 2_FUNDAMENTOS/9_lendo_input/index.js
@@ -1,9 +1,11 @@
+//O módulo readline é um módulo nativo do Node.js que permite ler inputs de entrada do usuário de forma interativa. Ele é útil para criar interfaces de linha de comando (CLI) e coletar informações do usuário durante a execução de um programa.
+
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question(`Qual a sua linguagem preferida? `, (language) => {
-  console.log(`A minha linguagem preferida é: ${language}`);
+readline.question(`Qual a sua linguagem preferida? `, (resposta) => {
+  console.log(`A minha linguagem preferida é: ${resposta}`);
   readline.close();
 });

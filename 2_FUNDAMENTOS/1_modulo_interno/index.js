ea8e8a4307526dd709bac50a3b4c1a33106faa59
Marcio Junior <mps.junior@hotmail.com>
2026-06-22 19:40:31 -0300

    alterações em modulos externos

--- 2_FUNDAMENTOS/1_modulo_interno/index.js
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -1,4 +1,4 @@
 const meuModulo = require('./meu_modulo')
 const soma = meuModulo.soma
 
-soma(2, 3)
+soma(2, 5)
--- 2_FUNDAMENTOS/5_modulos_externos/index.js
+++ 2_FUNDAMENTOS/5_modulos_externos/index.js
@@ -1,12 +1,28 @@
-// --nome=Matheus --idade=30
+// O módulo minimist também é utilizado para capturar argumentos passados via linha de comando. Porém de uma forma mais simples e direta, sem a necessidade de criar um array de argumentos.
+//Basta colocar estes dois traços antes dos nomes dos argumentos, como no exemplo a seguir: "node index.js --nome=Marcio --idade=39"
+
 const minimist = require("minimist");
 
-const args = minimist(process.argv.slice(2));
+const args = minimist(process.argv);
 
-console.log(args);
-
-const nome = args["nome"];
-const idade = args["idade"];
+const nome = args.nome;
+const idade = args.idade;
 
 console.log(nome);
 console.log(idade);
+
+
+// Também é possível estruturar o código desta maneira abaixo. Porém, ela é uma forma um pouco mais verbosa.
+// Da mesma forma, é necessário colocar os dois traços antes dos nomes dos argumentos, como no exemplo a seguir: "node index.js --profissao=Programador --cidade=Rio"
+
+/*
+const args = minimist(process.argv.slice(2))
+
+console.log(args);
+
+const profissao = args["profissao"];
+const cidade = args["cidade"];
+
+console.log(profissao);
+console.log(cidade);
+*/

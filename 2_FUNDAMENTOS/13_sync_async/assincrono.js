52ca747a9868dec5758ee70ebbcf55c7d70287f8
Marcio Junior <mps.junior@hotmail.com>
2026-06-22 20:49:01 -0300

    adição de comentário explicativo do código

--- 2_FUNDAMENTOS/13_sync_async/assincrono.js
+++ 2_FUNDAMENTOS/13_sync_async/assincrono.js
@@ -1,3 +1,7 @@
+//O código abaixo é um exemplo de escrita assíncrona em Node.js. A função `writeFile` não bloqueia a execução do código, permitindo que o próximo console.log seja executado imediatamente, enquanto a escrita do arquivo ocorre em segundo plano. A função de callback é chamada quando a escrita do arquivo é concluída, permitindo que você execute ações adicionais após a criação do arquivo.
+
+//O módulo `fs` (file system) é utilizado para interagir com o sistema de arquivos, permitindo a leitura e escrita de arquivos de forma síncrona ou assíncrona.
+
 const fs = require("fs");
 
 console.log("Início");
--- 2_FUNDAMENTOS/13_sync_async/sincrono.js
+++ 2_FUNDAMENTOS/13_sync_async/sincrono.js
@@ -1,3 +1,7 @@
+//O código abaixo é um exemplo de escrita síncrona em Node.js. A função `writeFileSync` bloqueia a execução do código até que o arquivo seja completamente escrito, o que significa que o próximo console.log só será executado após a conclusão da escrita do arquivo.
+
+//O módulo `fs` (file system) é utilizado para interagir com o sistema de arquivos, permitindo a leitura e escrita de arquivos de forma síncrona ou assíncrona.
+
 const fs = require("fs");
 
 console.log("Início");

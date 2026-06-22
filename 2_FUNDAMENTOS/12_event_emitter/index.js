16455f19b80a734b38d6ec9441a329c51b65c1d1
Marcio Junior <mps.junior@hotmail.com>
2026-06-22 20:42:38 -0300

    adicionando comentário explicativo sobre EventEmitter

--- 2_FUNDAMENTOS/12_event_emitter/index.js
+++ 2_FUNDAMENTOS/12_event_emitter/index.js
@@ -1,3 +1,5 @@
+// O EventEmitter é uma classe do Node.js que permite criar e gerenciar eventos personalizados. Ele é amplamente utilizado para lidar com eventos assíncronos, como cliques de mouse, conexões de rede, ou qualquer outra ação que possa ocorrer em um aplicativo. O EventEmitter fornece uma maneira de registrar ouvintes (listeners) para eventos específicos e emitir esses eventos quando necessário. Isso é útil para criar aplicativos mais flexíveis e responsivos, permitindo que diferentes partes do código se comuniquem de forma eficiente.
+
 const EventEmitter = require('events')
 const eventEmitter = new EventEmitter()
 

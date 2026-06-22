6655b964b68539f71d77bff794ada60c0838b5fb
Marcio Junior <mps.junior@hotmail.com>
2026-06-22 19:48:37 -0300

    adicionando comentário

--- 2_FUNDAMENTOS/8_melhorando_visual/index.js
+++ 2_FUNDAMENTOS/8_melhorando_visual/index.js
@@ -1,9 +1,12 @@
+//O módulo chalk permite que você estilize o texto no console, adicionando cores, estilos e fundos. No exemplo abaixo, usamos o chalk para exibir mensagens diferentes com base na nota do aluno.
+
 const chalk = require("chalk");
 
-const nota = 8;
+const nota = 10;
 
 if (nota >= 7) {
   console.log(chalk.green.bold("Parabéns, você passou!"));
 } else {
   console.log(chalk.bgRed.black("Você precisa fazer a prova final!"));
 }
+

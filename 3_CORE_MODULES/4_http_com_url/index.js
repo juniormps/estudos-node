2bdd1e6da0c4f33c4cf4025b2949446ea6c82990
Marcio Junior <mps.junior@hotmail.com>
2026-06-24 01:47:20 -0300

    adição de comentário explicativo

--- 3_CORE_MODULES/4_http_com_url/index.js
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -1,24 +1,27 @@
+//Utilizando o módulo http e url para criar um servidor que recebe parâmetros via query string. O url é utilizado para fazer o parse da url e extrair os parâmetros passados na query string. O servidor responde com uma mensagem de boas-vindas personalizada, caso o parâmetro "name" seja fornecido, ou solicita que o usuário preencha seu nome caso contrário.
+
 const http = require("http");
 const url = require("url");
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  var urlInfo = require("url").parse(req.url, true);
-  const name = urlInfo.query.name;
+    var urlInfo = url.parse(req.url, true);
 
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
+    const name = urlInfo.query.name;
 
-  if (!name) {
-    res.end(
-      "<h1>Preencha seu nome:</h1><form method='GET'><input type='text' name='name'/><input type='submit' value='Enviar'></form>"
-    );
-  } else {
-    res.end(`<h1>Seja bem-vindo ${name}!</h1>`);
-  }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    if (!name) {
+        res.end(
+            "<h1>Preencha seu nome:</h1><form method='GET'><input type='text' name='name'/><input type='submit' value='Enviar'></form>",
+        );
+    } else {
+        res.end(`<h1>Seja bem-vindo ${name}!</h1>`);
+    }
 });
 
 server.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`);
+    console.log(`Servidor rodando na porta: ${port}`);
 });
